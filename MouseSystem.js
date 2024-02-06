71825b961a0439236b2b3381dc8648740d9b1f30
Semyon <recksuper1@gmail.com>
2024-02-06 13:01:53 +0500

    [1.1] Update
    Check plugin description

--- MouseSystem.js
+++ MouseSystem.js
@@ -1,7 +1,3 @@
-//-----------------------------------------------------------------------------
-// Click Activation Plugin for RPG Maker MZ
-//-----------------------------------------------------------------------------
-
 var Imported = Imported || {};
 Imported.RusShm_ClickActivation = true;
 
@@ -11,7 +7,7 @@ RusShm.CA.pluginName = "MouseSystem";
 
 //-----------------------------------------------------------------------------
 /*:
- * @plugindesc Allows events to be activated with both left and right clicks and triggers actions on mouse hover.
+ * @plugindesc [1.1] Allows events to be activated with both left and right clicks and triggers actions on mouse hover.
  * @url https://boosty.to/russhm
  * @target MZ
  * @author RusShm
@@ -25,23 +21,45 @@ RusShm.CA.pluginName = "MouseSystem";
  *
  * Context Menu:
  * If the setting is true, then if the player clicks right mouse button on the event with "<right_click_activate>" tag, the choices would appear on the cursor
+ * 
+ * Custom cursor:
+ * You can change cursor default cursor. You can set Hover Icon for the event by using plugin command.
+ *
+ * Plugin parameters:
+ * Activation Variable: variable to store the last activation type. May be useful if you want to make different things by different clicks.
+ *
+ * Context Menu: Shows choice menu around mouse when event activated by right click
+ *
+ * Use custom cursors?: Determines if you need the custom cursors in your game 
+ *
+ * Custom cursor Image: Set the custom image for the cursor. Image should be in /img/system/ folder.
+ *
+ * Plugin Commands:
+ *
+ * Set hover Icon: Sets the cursor image which should be used when mouse hovered over event
+ *
  * @param activationVariable
- * @name Activation Variable
+ * @text Activation Variable
  * @desc The variable that will be set to indicate the activation type.
  * @type variable
  * @default 1
  *
  * @param contMenu
- * @name Context menu (4 choices)
+ * @text Context menu
  * @desc Shows choice menu around mouse when activated by right click
  * @type boolean
  * @default false
+ *
+ * @param useCustomCursor
+ * @text Use custom cursors?
+ * @type boolean
+ * @desc If set to true custom cursors would be used 
  *
  * @param customCursor
  * @text Custom Cursor Image
  * @type file
  * @dir img/system/
- * @desc Set the custom image for the cursor. Leave blank to use the default system cursor.
+ * @desc Set the custom image for the cursor. Image should be in /img/system/ folder
  *
  * @command setHoverIcon
  * @text Set hover Icon
@@ -62,7 +80,7 @@ const base_url = "./img/system/";
 RusShm.CA.params = PluginManager.parameters(RusShm.CA.pluginName);
 RusShm.CA.activationVariable = Number(RusShm.CA.params["activationVariable"]);
 RusShm.CA.contMenu = String(RusShm.CA.params["contMenu"]);
-
+RusShm.CA.uCC = String(RusShm.CA.params["useCustomCursor"]);
 RusShm.CA.customCursor = String(RusShm.CA.params["customCursor"]);
 
 var isLoaded = false;
@@ -84,7 +102,7 @@ document.addEventListener("mousedown", function (event) {
 
 document.addEventListener("mousemove", function (event) {
 	if (cursorChanged == true){changeCursorIcon(RusShm.CA.customCursor);}
-	cursorMove()
+	if (RusShm.CA.uCC)cursorMove();
 	
     handleEventActivation("mouse_hover_activate");
 });
@@ -95,24 +113,24 @@ function cursorMove(){
 		
 		
 		
-    const tileX = $gameMap.canvasToMapX(TouchInput.x);
-    const tileY = $gameMap.canvasToMapY(TouchInput.y);
-    $gameMap.eventsXy(tileX, tileY).forEach(function (event) {
-        if (!event || !event.page()) {
-			changeCursorIcon(RusShm.CA.customCursor)
-			return;}
-		for (let i = 0; i < event.page().list.length; i++) {
-			if (event.page().list[i].code === 657 && event.page().list[i].parameters[0].match("Set Hover Icon")) {
-				let thing = event.page().list[i].parameters[0];
-				thing = thing.split("=");	
-				thing[1] = thing[1].substring(1);
-				changeCursorIcon(thing[1]);
-				cursorChanged = true;
-				}
-				
-			};
-		
-    });
+		const tileX = $gameMap.canvasToMapX(TouchInput.x);
+		const tileY = $gameMap.canvasToMapY(TouchInput.y);
+		$gameMap.eventsXy(tileX, tileY).forEach(function (event) {
+			if (!event || !event.page()) {
+				return;}
+			if (RusShm.CA.uCC == "true"){
+				for (let i = 0; i < event.page().list.length; i++) {
+					if (event.page().list[i].code === 657 && event.page().list[i].parameters[0].match("Set Hover Icon")) {
+						let thing = event.page().list[i].parameters[0];
+						thing = thing.split("=");	
+						thing[1] = thing[1].substring(1);
+						changeCursorIcon(thing[1]);
+						cursorChanged = true;
+						}
+						
+					};
+			}
+		});
 	}
 }
 
@@ -126,7 +144,7 @@ function changeCursorIcon(text){
 RusShm.CA.Game_Map_setup = Game_Map.prototype.setup;
 Game_Map.prototype.setup = function(mapId) {
 	RusShm.CA.Game_Map_setup.call(this,mapId);
-    changeCursorIcon(RusShm.CA.customCursor);
+    if (RusShm.CA.uCC == "true")changeCursorIcon(RusShm.CA.customCursor);
 	isLoaded = true;
 };
 
